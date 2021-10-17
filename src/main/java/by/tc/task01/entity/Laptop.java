48febde137fabca0d69e6c3e9504b82129370e6d
MaksimovaNatalya <ps_nat98@mail.ru>
2021-10-17 13:13:38 +0300

    1

--- src/main/java/by/tc/task01/entity/Laptop.java
+++ src/main/java/by/tc/task01/entity/Laptop.java
@@ -2,4 +2,73 @@ package by.tc.task01.entity;
 
 public class Laptop extends Appliance{
 	// you may add your own code here
+
+
+    private double batteryCapacity;
+    private String os;
+    private int memoryRom;
+    private int systemMemory;
+    private double cpu;
+    private int displayInchs;
+
+    public Laptop () {
+
+    }
+
+    public Laptop(double batteryCapacity, String os, int memoryRom, int systemMemory, double cpu, int displayInchs) {
+        this.batteryCapacity = batteryCapacity;
+        this.os = os;
+        this.memoryRom = memoryRom;
+        this.systemMemory = systemMemory;
+        this.cpu = cpu;
+        this.displayInchs = displayInchs;
+    }
+
+    public double getBatteryCapacity() {
+        return batteryCapacity;
+    }
+
+    public void setBatteryCapacity(float batteryCapacity) {
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+    public int getMemoryRom() {
+        return memoryRom;
+    }
+
+    public void setMemoryRom(int memoryRom) {
+        this.memoryRom = memoryRom;
+    }
+
+    public int getSystemMemory() {
+        return systemMemory;
+    }
+
+    public void setSystemMemory(int systemMemory) {
+        this.systemMemory = systemMemory;
+    }
+
+    public double getCpu() {
+        return cpu;
+    }
+
+    public void setCpu(float cpu) {
+        this.cpu = cpu;
+    }
+
+    public float getDisplayInchs() {
+        return displayInchs;
+    }
+
+    public void setDisplayInchs(int displayInchs) {
+        this.displayInchs = displayInchs;
+    }
 }
