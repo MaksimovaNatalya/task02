86417589410589a8f388d428a9b63258dd90b29f
MaksimovaNatalya <ps_nat98@mail.ru>
2021-10-17 13:41:45 +0300

    added constructors and getters setters to classes

--- src/main/java/by/tc/task01/entity/Refrigerator.java
+++ src/main/java/by/tc/task01/entity/Refrigerator.java
@@ -2,4 +2,72 @@ package by.tc.task01.entity;
 
 public class Refrigerator extends Appliance{
 	// you may add your own code here
+
+    private int powerConsumption;
+    private double weight;
+    private double freezerCapacity;
+    private double overallCapacity;
+    private double height;
+    private double width;
+
+    public Refrigerator () {
+
+    }
+
+    public Refrigerator(int powerConsumption, double weight, double freezerCapacity, double overallCapacity, double height, double width) {
+        this.powerConsumption = powerConsumption;
+        this.weight = weight;
+        this.freezerCapacity = freezerCapacity;
+        this.overallCapacity = overallCapacity;
+        this.height = height;
+        this.width = width;
+    }
+
+    public int getPowerConsumption() {
+        return powerConsumption;
+    }
+
+    public void setPowerConsumption(int powerConsumption) {
+        this.powerConsumption = powerConsumption;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getFreezerCapacity() {
+        return freezerCapacity;
+    }
+
+    public void setFreezerCapacity(int freezerCapacity) {
+        this.freezerCapacity = freezerCapacity;
+    }
+
+    public double getOverallCapacity() {
+        return overallCapacity;
+    }
+
+    public void setOverallCapacity(double overallCapacity) {
+        this.overallCapacity = overallCapacity;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
 }
--- src/main/java/by/tc/task01/entity/TabletPC.java
+++ src/main/java/by/tc/task01/entity/TabletPC.java
@@ -1,5 +1,62 @@
 package by.tc.task01.entity;
 
-public class TabletPC extends Appliance{
-	// you may add your own code here
+public class TabletPC extends Appliance {
+    // you may add your own code here
+    private double batteryCapacity;
+    private int displayInches;
+    private int memoryRom;
+    private int flashMemoryCapacity;
+    private String color;
+
+    public TabletPC() {
+
+    }
+
+    public TabletPC(double batteryCapacity, int displayInches, int memoryRom, int flashMemoryCapacity, String color) {
+        this.batteryCapacity = batteryCapacity;
+        this.displayInches = displayInches;
+        this.memoryRom = memoryRom;
+        this.flashMemoryCapacity = flashMemoryCapacity;
+        this.color = color;
+    }
+
+    public double getBatteryCapacity() {
+        return batteryCapacity;
+    }
+
+    public void setBatteryCapacity(double batteryCapacity) {
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    public int getDisplayInches() {
+        return displayInches;
+    }
+
+    public void setDisplayInches(int displayInches) {
+        this.displayInches = displayInches;
+    }
+
+    public int getMemoryRom() {
+        return memoryRom;
+    }
+
+    public void setMemoryRom(int memoryRom) {
+        this.memoryRom = memoryRom;
+    }
+
+    public int getFlashMemoryCapacity() {
+        return flashMemoryCapacity;
+    }
+
+    public void setFlashMemoryCapacity(int flashMemoryCapacity) {
+        this.flashMemoryCapacity = flashMemoryCapacity;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
